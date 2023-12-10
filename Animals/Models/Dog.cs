3b3ef8dc61b8192e7a21a28c50d0526980f68e8f
qatestst <134833768+qatestst@users.noreply.github.com>
2023-12-10 21:09:25 +0200

    Add project files.

---
+++ Animals/Models/Dog.cs
@@ -0,0 +1,19 @@
+﻿using Animals.Models;
+
+internal class Dog : Animal
+{
+    private string name;
+    private string favoriteFood;
+
+    public Dog(string name, string favoriteFood) :base(name, favoriteFood)
+    {
+    }
+
+        public override string ExplainSelf()
+    {
+        return base.ExplainSelf() + "\nBORK";
+    }
+
+    
+
+}
